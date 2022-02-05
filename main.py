137e4a117db19b4446bafe3aea99c8b5096f5990
triqqs <annkal6@st.amu.edu.pl>
2022-02-05 19:09:19 +0100

    wczytanie argumentacji dotyczącej zdrowia i w niej wyszukanie słów emotywnych

--- main.py
+++ main.py
@@ -4,11 +4,19 @@ import string
 
 nlp = sp.load("pl_core_news_lg")  # wczytanie polskiej bazy
 
-data = pd.read_csv("./dane/nawl_emo_cat.csv", delimiter=";")
+data = pd.read_csv("./dane/nawl_val_aro.csv", delimiter=";")
 nawl_words = data.NAWL_word  # wczytanie słów z bazy NAWL
 
-my_words = ["wiosna", "miły", "chleb", "szczepionka", "złodziej", "pies", "absurd", "bukiet", "mama", "żałoba", "impreza", "miła"]
-my_tekst = "Pies domowy – udomowiony gatunek ssaka drapieżnego z rodziny psowatych, traktowany przez niektóre ujęcia systematyczne za podgatunek wilka"
+argumentacje = pd.read_csv("./dane/Zadanie_październik_przesłanki.csv", delimiter=";", encoding="cp1250",
+                           usecols=["Konkluzja", "Przesłanka 1", "Przesłanka 2", "Przesłanka 3", "Przesłanka 4", "Przesłanka 5"])
+
+
+konk = argumentacje["Konkluzja"].values
+p1 = argumentacje["Przesłanka 1"].values
+p2 = argumentacje["Przesłanka 2"].values
+p3 = argumentacje["Przesłanka 3"].values
+p4 = argumentacje["Przesłanka 4"].values
+p5 = argumentacje["Przesłanka 5"].values
 
 
 def my_matcher(mylist, emolist):  # zwraca część spólną dwóch list (bez powtórzeń)
@@ -28,8 +36,25 @@ def lemmatizer(mylist):  # zwraca wyrazy zmienione do formy podstawowej
     return my_lemma
 
 
-nawl_match = my_matcher(lemmatizer(my_words), nawl_words)
-print(nawl_match)
+def list_matcher(my_lists, emo_list):
+    result = []
+    for l in my_lists:
+        if type(l) == float:
+            continue
+        l = lemmatizer(tokenizer(l))
+        result += [my_matcher(l, emo_list)]
+    return result
 
-nawl_match = my_matcher(lemmatizer(tokenizer(my_tekst)), nawl_words)
+
+nawl_match = list_matcher(konk, nawl_words)
+print(nawl_match)
+nawl_match = list_matcher(p1, nawl_words)
+print(nawl_match)
+nawl_match = list_matcher(p2, nawl_words)
+print(nawl_match)
+nawl_match = list_matcher(p3, nawl_words)
+print(nawl_match)
+nawl_match = list_matcher(p4, nawl_words)
+print(nawl_match)
+nawl_match = list_matcher(p5, nawl_words)
 print(nawl_match)
