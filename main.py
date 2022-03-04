92b7289ab2ca28b3ea5faf590b35fbbf80ae635d
triqqs <annkal6@st.amu.edu.pl>
2022-03-04 18:26:04 +0100

    1. Dodatnie bazy argumentów dotyczących zdrowia. 2. - sprawdzanie walencji dla poszczególnych słów z argumentacji dot zdrowia [word_valence]    - sprawdzanie walencji dla wszystkich słów emotywnych w danym zdaniu (konkluzji, przesłance) [sentence_valence]    - sprawdzanie średniej walencji dla wszystkich słów emotywnych w zdaniach (konkluzjach, przesłankach) [zawarte w funckji print_matched_sentences]

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import pandas as pd
 import spacy as sp
 import string
+from statistics import mean
 
 nlp = sp.load("pl_core_news_lg")  # wczytanie polskiej bazy
 
-data = pd.read_csv("./dane/nawl_val_aro.csv", delimiter=";")
+data = pd.read_csv("./dane/nawl_val_aro.csv", delimiter=";", decimal=',')
+data['val_M_all'] = data['val_M_all'].astype(float)
+
 nawl_words = data.NAWL_word  # wczytanie słów z bazy NAWL
 
-argumentacje = pd.read_csv("./dane/Zadanie_październik_przesłanki.csv", delimiter=";", encoding="cp1250",
-                           usecols=["Konkluzja", "Przesłanka 1", "Przesłanka 2", "Przesłanka 3", "Przesłanka 4", "Przesłanka 5"])
-
+argumentacje = pd.read_csv("./dane/Baza_arg_dot_zdrowia.csv", delimiter=",",
+                           usecols=["Konkluzja", "Przesłanka 1", "Przesłanka 2", "Przesłanka 3", "Przesłanka 4",
+                                    "Przesłanka 5", "Przesłanka 6", "Przesłanka 7", "Przesłanka 8"])
 
 konk = argumentacje["Konkluzja"].values
 p1 = argumentacje["Przesłanka 1"].values
@@ -17,6 +20,9 @@ p2 = argumentacje["Przesłanka 2"].values
 p3 = argumentacje["Przesłanka 3"].values
 p4 = argumentacje["Przesłanka 4"].values
 p5 = argumentacje["Przesłanka 5"].values
+p6 = argumentacje["Przesłanka 6"].values
+p7 = argumentacje["Przesłanka 7"].values
+p8 = argumentacje["Przesłanka 8"].values
 
 
 def my_matcher(mylist, emolist):  # zwraca część spólną dwóch list (bez powtórzeń)
@@ -46,15 +52,40 @@ def list_matcher(my_lists, emo_list):
     return result
 
 
-nawl_match = list_matcher(konk, nawl_words)
-print(nawl_match)
-nawl_match = list_matcher(p1, nawl_words)
-print(nawl_match)
-nawl_match = list_matcher(p2, nawl_words)
-print(nawl_match)
-nawl_match = list_matcher(p3, nawl_words)
-print(nawl_match)
-nawl_match = list_matcher(p4, nawl_words)
-print(nawl_match)
-nawl_match = list_matcher(p5, nawl_words)
-print(nawl_match)
+def word_valence(my_word, emo_df):
+    return emo_df.loc[emo_df['NAWL_word'] == my_word, 'val_M_all'].iloc[0]
+
+
+def sentence_valence(my_sentence, emo_df):
+    if len(my_sentence) == 0:
+        return None
+    result = []
+    for word in my_sentence:
+        result += [word_valence(word, emo_df)]
+    return mean(result)
+
+
+def print_matched_sentences(sentences_list, emo_df):
+    nawl_match = list_matcher(sentences_list, emo_df.NAWL_word)
+    valences = []
+    for sentence in nawl_match:
+        result = sentence_valence(sentence, data)
+        if result:
+            valences += [result]
+        print(result, end='\t')
+    print()
+    print(nawl_match)
+    print(mean(valences))
+    print()
+
+
+print_matched_sentences(konk, data)
+print_matched_sentences(p1, data)
+print_matched_sentences(p2, data)
+print_matched_sentences(p3, data)
+print_matched_sentences(p4, data)
+print_matched_sentences(p5, data)
+print_matched_sentences(p6, data)
+print_matched_sentences(p7, data)
+print_matched_sentences(p8, data)
+
